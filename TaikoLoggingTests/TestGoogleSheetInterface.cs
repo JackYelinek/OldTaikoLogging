3d1dbc4dd68fed05048b6155b3b7f1c878c2938e
Jackson Yelinek <jackyelinek@gmail.com>
2019-08-13 15:52:29 -0400

    Updated GoogleSheetInterface Functions to make them more adaptable and less likely to mess up from an update. Didn't get to finish the RankedMatch area of things just now. Program's broken with this commit, which is probably a horrible idea to let happen, but I don't have time to fix it right now.

---
+++ TaikoLoggingTests/TestGoogleSheetInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaikoLoggingTests
+{
+    class TestGoogleSheetInterface
+    {
+        // I have some plans to implement this, but I will wait to do that until later
+
+
+    }
+}
